11eb51c6c895b026dd930cf492f4702561d34c6c
billan <billan@outlook.com>
2015-04-14 02:22:35 -1000

    Working

--- Index.php
+++ Index.php
@@ -14,26 +14,27 @@ ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
 
-//Includes
-require 'dbconn.php';
+function datagetfunction () {
+	//Includes (move outside of function, will need to pass connection object to function)
+	require 'dbconn.php';
 
-//Array to store DB results
-$resultarray = resultarray();
+		//Array to store DB results
+		//$resultarray = resultarray();
 
-$sql = "SELECT * FROM loctbl INNER JOIN costtbl ON costtbl.locid = loctbl.pkey";
-$result = $conn->query($sql);
-
-//Build array from $sql
-while($row = mysql_fetch_assoc($sql)){
-// add each row returned into an array
-$resultarray[] = $row;
+		$sql = "SELECT * FROM loctbl INNER JOIN costtbl ON costtbl.locid = loctbl.pkey";
+		$result = $conn->query($sql);
+		//print_r($result);
+		//Build array from $sql
+		while($row = mysqli_fetch_assoc($result)){
+		//Add each row returned into an array
+		$resultarray[] = $row;
+		}
+	Return $resultarray;
+	$conn->close();
 }
+$results = datagetfunction();
+print_r($results);
 
-Return $resultarray;
-
-print_r($resultarray);
-
-$conn->close();
 ?>  
 
 </body>
